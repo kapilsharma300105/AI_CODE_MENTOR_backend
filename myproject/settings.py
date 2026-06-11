fdcd3d6de4c02ed5b54802ecf06cd2b6921dd4a1
priyanshi <priyanshikapil300105@gmail.com>
2026-06-11 22:52:30 +0530

    Fix HTTPS redirect for Google OAuth

--- myproject/settings.py
+++ myproject/settings.py
@@ -270,7 +270,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
