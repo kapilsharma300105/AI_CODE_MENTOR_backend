fdb9bf8aff1182c84aad118ca53fd5985da5366b
priyanshi <priyanshikapil300105@gmail.com>
2026-06-12 01:46:01 +0530

    fix: auto set site domain on startup

--- myapp/apps.py
+++ myapp/apps.py
@@ -6,3 +6,18 @@ class MyappConfig(AppConfig):
 
     def ready(self):
         import myapp.signals   # ✅ yahi important hai
+        from django.apps import AppConfig
+
+class MyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myapp'
+
+    def ready(self):
+        try:
+            from django.contrib.sites.models import Site
+            site, _ = Site.objects.get_or_create(id=1)
+            site.domain = "ai-code-mentor-backend-0rmn.onrender.com"
+            site.name = "AI Code Mentor"
+            site.save()
+        except:
+            pass
